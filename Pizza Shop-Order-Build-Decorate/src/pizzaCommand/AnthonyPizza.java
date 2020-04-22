f4c1ef804f16448ce3d44c66d315aacaf24e4f0b
Anthony Connor <ajc157@txstate.edu>
2020-04-22 18:10:51 -0500

    AnthonyPizzaCommand

---
+++ Pizza Shop-Order-Build-Decorate/src/pizzaCommand/AnthonyPizza.java
@@ -0,0 +1,19 @@
+package patternsexamp;
+
+import java.awt.*;        // Using AWT container and component classes
+import java.awt.event.*;  // Using AWT event classes and listener interfaces
+
+// Using Command Design Patttern (code based on DZone Command Pattern)
+
+
+//Concrete Command
+public class AnthonyPizza implements PizzaCommand{
+  //reference to the order
+  PizzaOrder order;
+  public AnthonyPizza(PizzaOrder order){
+    this.order = order;
+  }
+  public void execute(){
+    order.bakeAnthonyPizza();
+  }
+}
