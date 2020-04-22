089028c281ece5fea2c060e1b2ec00d2bcdc182d
Mason <43713560+mercurrie@users.noreply.github.com>
2020-04-22 18:20:44 -0500

    pizza file update
    masonpizza command added
    pizza orderer changed
    pizza order changed

--- Pizza Shop-Order-Build-Decorate/src/pizzaBuilder/PizzaOrder.java
+++ Pizza Shop-Order-Build-Decorate/src/pizzaBuilder/PizzaOrder.java
@@ -12,22 +12,52 @@ public class PizzaOrder{
   public Pizza p;
 
   public void bakeDeepDish(){
-	System.out.format("Baking a deepdish... \n"); 
-    pb = new Pizza.Builder("Deep Dish"); 
+	System.out.format("Baking a deepdish... \n");
+    pb = new Pizza.Builder("Deep Dish");
     p = pb.pizzas(1).build();
     reportOrder();
 
   }
   public void bakeThinCrust(){
   	System.out.format("Baking a thincrust... \n");
-  	pb = new Pizza.Builder("Thin Crust");  
+  	pb = new Pizza.Builder("Thin Crust");
     p = pb.pizzas(1).build();
     reportOrder();
   }
 
+    public void bakeMasonPizza(){
+        System.out.format("Baking a Mason pizza... \n");
+        pb = new Pizza.Builder("Mason");
+        p = pb.pizzas(1).build();
+        reportOrder();
+    }
+
+    public void bakeAnthonyPizza(){
+        System.out.format("Baking a Anthony pizza... \n");
+        pb = new Pizza.Builder("Anthony");
+        p = pb.pizzas(1).build();
+        reportOrder();
+    }
+
+    public void bakeCraigPizza(){
+        System.out.format("Baking a Craig pizza... \n");
+        pb = new Pizza.Builder("Craig");
+        p = pb.pizzas(1).build();
+        reportOrder();
+    }
+
+    public void bakeSeanPizza(){
+        System.out.format("Baking a Sean pizza... \n");
+        pb = new Pizza.Builder("Sean");
+        p = pb.pizzas(1).build();
+        reportOrder();
+    }
+
+
+
   private void reportOrder() {
-      System.out.format("\nBuilt %s - num pies: %d, pepperoni: %b, onion: %b, frenchfries %b\n", 
-    	p.getName(),  
+      System.out.format("\nBuilt %s - num pies: %d, pepperoni: %b, onion: %b, frenchfries %b\n",
+    	p.getName(),
       p.getNumber(),
       p.toppings.pepperoni,
       p.toppings.onion,
